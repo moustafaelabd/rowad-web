ef02e19e11fc823302b4a0215aa0eacf54ddbc06
moustafaelabd <abdallahelabd22@gmail.com>
2026-08-29 22:57:37 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,10 +1,22 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "rowad.sqlite",
-  logging: false,
-});
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      protocol: "postgres",
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize({
+      dialect: "sqlite",
+      storage: "rowad.sqlite",
+      logging: false,
+    });
 
 // =========================
 // Admin
