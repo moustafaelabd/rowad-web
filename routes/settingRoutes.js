ea6333bce340a5ea8673603d1f3deb4fcadb4d16
moustafaelabd <abdallahelabd22@gmail.com>
2026-08-30 00:35:59 +0300

    Update settingRoutes.js

--- routes/settingRoutes.js
+++ routes/settingRoutes.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const multer = require("multer");
+const upload = require("../middleware/upload"); // نفس الملف اللي بيستخدمه المنتجات
 const settingsController = require("../controllers/settingController"); // من غير s
-// خزّن الصور في public/uploads زي باقي الموديولز عندك
-const upload = multer({ dest: "public/uploads/" });
 
 router.get("/", settingsController.getSettings);
 router.put("/", upload.single("logo"), settingsController.updateSettings);
